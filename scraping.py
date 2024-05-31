bf886636d6b9338fe13d8ab0b3a3b709e78a472a
MateusRanzani <mateusranzani@hotmail.com>
2024-05-30 22:21:17 -0300

    create find product

--- scraping.py
+++ scraping.py
@@ -5,3 +5,25 @@ import pandas as pd
 import math
 
 url = 'https://www.kabum.com.br/espaco-gamer/cadeiras-gamer'
+
+headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+    (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+
+site = requests.get(url, headers=headers)
+soup = BeautifulSoup(site.content, 'html.parser')
+qtd_itens = soup.find('div', id='listingCount').get_text().strip()
+index = qtd_itens.find(' ')
+qtd = qtd_itens[:index]
+
+ultima_pagina = math.ceil(int(qtd)/ 20)
+dic_produtos = { 'marca':[], 'preco':[] }
+
+for i in range(1, ultima_pagina+1):
+    url_pag = f'https://www.kabum.com.br/espaco-gamer/cadeiras-gamer?page_number={i}&page_size=20&facet_filters=&sort=most_searched'
+    site = requests.get(url_pag, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    produtos = soup.find_all('div', class_= re.compile('productCard'))
+
+    for produto in produtos:
+        marca = produto.find('span', class_=re.compile('nameCard')).get_text().strip()
+        preco = produto.find('span', class_=re.compile('nameCard')).get_text().strip()
